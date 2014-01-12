bc767ce950cf370485db4104c8011325a142ce4a
timshim <timshim@gmail.com>
2014-01-12 14:12:29 +0800

    Initial Commit

---
+++ SimpleWeather/SWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SWAppDelegate.h
+//  SimpleWeather
+//
+//  Created by Tim Shim on 12/1/14.
+//  Copyright (c) 2014 Tim Shim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SimpleWeather/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SimpleWeather
+//
+//  Created by Tim Shim on 12/1/14.
+//  Copyright (c) 2014 Tim Shim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SWAppDelegate class]));
+    }
+}
